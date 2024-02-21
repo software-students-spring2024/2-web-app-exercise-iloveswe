5b4b58f87db1a4183ce679e88f06000ed216cda6
jladrover <ja4146@nyu.edu>
2024-02-21 18:09:29 -0500

    connecting db

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
+import os
 from flask import Flask, request, jsonify
 import pandas as pd
-# Import the function to handle model predictions
 from model_handlers import get_prediction
+import pymongo
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+
+# Connect to the MongoDB server using environment variables 
+load_dotenv()
+connection = pymongo.MongoClient(os.getenv('MONGO_URI'))
+db = connection[os.getenv('MONGO_DBNAME')]
+try:
+    # verify the connection works by pinging the database
+    connection.admin.command("ping")  
+    print(" *", "Connected to MongoDB!")  
+except Exception as e:
+    # the ping command failed, so the connection is not available.
+    print(" * MongoDB connection error:", e)
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
