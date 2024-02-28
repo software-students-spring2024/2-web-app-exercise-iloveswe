290503a2d23a9329dac39950dbacdedd9f0dc208
joshforlenza <joshforlenza@gmail.com>
2024-02-28 14:33:16 -0500

    Added upload file form and route

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ from model_handler import get_prediction
 # import user mode
 from User import *
 
+ALLOWED_EXTENSIONS = {'csv'}
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 app = Flask(__name__)
 app.secret_key = b'iloveswe'
 
@@ -133,6 +138,24 @@ def csvs():
     Shows a user's csvs
     """
     return render_template("csvs.html")
+
+@app.route('/csvs', methods=['POST'])
+def upload_file():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = file.filename
+        print(filename)
+        #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect('/csvs')
 
 @app.route('/editcsv/<post_id>')
 def edit_csv(post_id):
